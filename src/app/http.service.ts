a9884e4c84dd5ef8967a0035d0c0b5347e5dec83
Jun Lee <terry@terry.matchbox>
2016-10-09 13:22:22 +0800

    modi service

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http, Response } from '@angular/http';
+import { Headers, Http, Response, Jsonp, RequestOptions } from '@angular/http';
 import { ImageInfo } from './imageInfo';
 import { Observable }     from 'rxjs/Observable';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class HttpService {
@@ -10,23 +14,26 @@ export class HttpService {
 
   constructor(private http: Http) { }
 
-  // get all images from docker deamon
-  // getImageInfo(): Promise<ImageInfo[]> {
-  //   return this.http.get((this.address + this.imagesUrl), { headers: this.headers })
-  //     .toPromise()
-  //     .then(response => response.json().data as ImageInfo[])
-  //     .catch(this.handleError);
+  //get all image info from docker deamon
+  // getImageInfo() {
+  //   return this.jsonp
+  //     .get(this.address + this.imagesUrl)
+  //     .map(response => <string[]>response.json()[1]);
+  //
   // }
 
-  getImageInfo(): Observable<ImageInfo[]> {
+  getHeroes(): Promise<ImageInfo[]> {
+    //let headers = new Headers({ 'Access-Control-Allow-Origin': '*' });
+    //let options = new RequestOptions({ headers: headers });
     return this.http.get(this.address + this.imagesUrl)
-      .map(this.extractData)
+      .toPromise()
+      .then(this.extractData)
       .catch(this.handleError);
   }
 
   private extractData(res: Response) {
     let body = res.json();
-    return body.data || {};
+    return body.data || { "nima": "fadf" };
   }
   private handleError(error: any) {
     // In a real world app, we might use a remote logging infrastructure
@@ -34,6 +41,7 @@ export class HttpService {
     let errMsg = (error.message) ? error.message :
       error.status ? `${error.status} - ${error.statusText}` : 'Server error';
     console.error(errMsg); // log to console instead
-    return Observable.throw(errMsg);
+    return Promise.reject(errMsg);
   }
+
 }
--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -3,36 +3,30 @@ import { Image } from './image';
 import { IMAGELIST } from './mock-images';
 import { HttpService } from '../http.service';
 import {Jsonp} from '@angular/http';
-import { ImageInfo } from '../imageinfo';
+import { ImageInfo } from '../imageInfo';
+import { Observable }       from 'rxjs/Observable';
 
 @Component({
   selector: 'app-images',
   templateUrl: './images.component.html',
-  styleUrls: ['./images.component.css']
+  styleUrls: ['./images.component.css'],
+
 })
 export class ImagesComponent implements OnInit {
   appList: Image[];
-  httpService: HttpService;
   imageInfoes: ImageInfo[];
+  errorMessage: string;
 
-  constructor() { }
-
-  // ngOnInit() {
-  //   this.appList = IMAGELIST;
-  //   this.httpService.getImageInfo();
-  //   console.log("tmd");
-  //   this.httpService.getImageInfo().then(result => {
-  //     this.data = result;
-  //     console.log(this.data);
-  //   });
-  // }
+  constructor(private httpService: HttpService) { }
 
   ngOnInit() {
     this.appList = IMAGELIST;
 
-    this.httpService.getImageInfo()
-      .subscribe(
-      result => this.imageInfoes = result,
-    );
+    this.httpService.getHeroes()
+      .then(
+      images => this.imageInfoes = images,
+      error => this.errorMessage = <any>error);
+    console.log("nmd");
+    console.log(this.errorMessage);
   }
 }
