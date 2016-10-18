c2aaadc142585e53e03bc6a0bccb0f92b1ed67c4
MatchboxDorry <job@dorry.io>
2016-10-18 17:12:11 +0800

    start.sh added

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo ..........Restarting the docker service on your machine..........
+sudo service docker stop
+sudo dockerd -H unix:///var/run/docker.sock -H tcp://0.0.0.0:5000 --api-enable-cors=true
+echo ..........Starting the dorry-web app..........
+docker run -it -p 4200:4200 dorry-web
