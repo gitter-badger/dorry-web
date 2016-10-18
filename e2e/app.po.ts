7e13728e5ab3e1ecda49ac17f7a166229819c99d
MatchboxDorry <job@dorry.io>
2016-10-18 19:13:23 +0800

    e2e added

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,35 @@
 import { browser, element, by } from 'protractor/globals';
 
 export class DorryWebPage {
-  navigateTo() {
+  navigateToHome() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  navigateToService() {
+    return browser.get('/');
+  }
+
+  getMasthead() {
+    return element(by.css('app-masthead')).getText();
+  }
+
+  getFooter() {
+    return element(by.css('app-footer')).getText();
+  }
+
+  getErrorServiceHeader() {
+    return element(by.css('span.service-header-left')).getText();
+  }
+
+  getRemoveAll() {
+    return element(by.css('span.service-header-right')).getText();
+  }
+
+  getStoppedServiceHeader() {
+    return element(by.css('app-containers div:nth-child(2)')).getText();
+  }
+
+  getRunningServiceHeader() {
+    return element(by.css('app-containers div:nth-child(4)')).getText();
   }
 }
