7c11c330e20db8b1267cf880b80e4dcee1fbaaf8
Jun Lee <terry@terry.matchbox>
2016-10-20 14:09:04 +0800

    add karma converage

--- karma.conf.js
+++ karma.conf.js
@@ -1,21 +1,24 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', 'angular-cli'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-coverage'),
       require('karma-chrome-launcher'),
       require('karma-remap-istanbul'),
       require('angular-cli/plugins/karma')
     ],
-    files: [
-      { pattern: './src/test.ts', watched: false }
-    ],
+    files: [{
+      pattern: './src/test.ts',
+      watched: false
+    }],
     preprocessors: {
-      './src/test.ts': ['angular-cli']
+      './src/test.ts': ['angular-cli'],
+      'dist/**/!(*spec).js': ['coverage']
     },
     remapIstanbulReporter: {
       reports: {
@@ -23,11 +26,18 @@ module.exports = function (config) {
         lcovonly: './coverage/coverage.lcov'
       }
     },
+    coverageReporter: {
+      reporters: [{
+        type: 'json',
+        subdir: '.',
+        file: 'coverage-final.json'
+      }]
+    },
     angularCli: {
       config: './angular-cli.json',
       environment: 'dev'
     },
-    reporters: ['progress', 'karma-remap-istanbul'],
+    reporters: ['progress', 'karma-remap-istanbul', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
